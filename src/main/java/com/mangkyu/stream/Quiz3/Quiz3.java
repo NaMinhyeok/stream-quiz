bbeefc944ac91a1862c7262b33b769c0966f39ff
NaMinhyeok <nmh9097@gmail.com>
2024-07-30 09:14:51 +0900

    quiz3 : 3.2번 문제 풀이

--- src/main/java/com/mangkyu/stream/Quiz3/Quiz3.java
+++ src/main/java/com/mangkyu/stream/Quiz3/Quiz3.java
@@ -14,17 +14,20 @@ public class Quiz3 {
     // 3.1 모든 숫자 쌍의 배열 리스트를 반환하여라.
     // ex) numbers1 = [1,2,3], numbers2 = [3,4] -> [(1,3), (1,4), (2,3), (2,4), (3,3), (3,4)]
     public List<Integer[]> quiz1() {
-        numbers1.stream()
+        return numbers1.stream()
                 .flatMap(n -> numbers2.stream()
                         .map(m -> new Integer[]{n,m}))
                 .collect(Collectors.toList());
-        return Collections.emptyList();
     }
 
     // 3.2 모든 숫자 쌍의 곱이 가장 큰 값을 반환하여라.
     // ex) numbers1 = [1,2,3], numbers2 = [3,4] -> 12
     public int quiz2() {
-        return 0;
+        return numbers1.stream()
+                .flatMap(n -> numbers2.stream()
+                        .map(m->new Integer[]{n,m}))
+                .mapToInt(mul -> mul[0]*mul[1])
+                .max().orElse(0);
     }
 
 }
